b9ed9c43850d1902a84cd9b416c0671d13befc8d
kyamc <kyamc@zylun.com>
2017-08-18 19:01:22 +0800

    Fixed drawer issue,fixed responsive issue , added navigation menu

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="sideNav">
+    <v-navigation-drawer
+     temporary 
+     v-model="sideNav"
+      >
       <v-list>
-          <v-list-tile>
+          <v-list-tile v-for="item in menuItem" :key="item.title">
               <v-list-tile-action>
-                 <v-icon>supervisor_account</v-icon>
+                 <v-icon>{{ item.icon }}</v-icon>
               </v-list-tile-action>
-              <v-list-tile-content>View Meetups</v-list-tile-content>
+              <v-list-tile-content>{{ item.title }}</v-list-tile-content>
           </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar>
-      <v-toolbar-side-icon v-on:click ="sideNav = !sideNav"></v-toolbar-side-icon>
-      <v-toolbar-title>DevMeetup</v-toolbar-title>
+    <v-toolbar dark class="light-blue darken-4">
+      
+      <v-toolbar-title class="hidden-sm-and-up">
+        <v-toolbar-side-icon v-on:click.stop ="sideNav = !sideNav" ></v-toolbar-side-icon>
+      </v-toolbar-title>
+      <v-toolbar-title>
+         DevMeetup
+      </v-toolbar-title>
       
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-          <v-btn flat>
-            <v-icon left>supervisor_account</v-icon>
-            View Meetups
+      <v-toolbar-items class="hidden-xs-only">
+          <v-btn flat v-for="item in menuItem" :key="item.title">
+            <v-icon left dark>{{ item.icon }}</v-icon>
+            {{item.title}}
           </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -32,7 +40,12 @@
   export default {
     data () {
       return {
-        sideNav: false
+        sideNav: false,
+        menuItem: [ {icon: 'supervisor_account', title: 'View Meetups'},
+          {icon: 'room', title: 'Organize Meetup'},
+          {icon: 'person', title: 'Profile'},
+          {icon: 'face', title: 'Sign up'},
+          {icon: 'lock_open', title: 'Sign in'}]
       }
     }
   }
